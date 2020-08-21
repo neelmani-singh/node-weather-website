a219fc9f56f27f053b02d03d7a059421f52ef81f
Rishikesh Singh <neelmanisinghvns@gmail.com>
2020-08-21 21:54:22 +0530

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location!',undefined)
         } else{
-            callback(undefined,body.current.weather_descriptions[0] + '. it is currently '+ body.current.temperature + ' degrees celsius out. It feels like ' + body.current.feelslike + ' degrees celsius out.')
+            callback(undefined,body.current.weather_descriptions[0] + '. it is currently '+ body.current.temperature + ' degrees celsius out. It feels like ' + body.current.feelslike + ' degrees celsius out. The humidity is ' + body.current.humidity + ' %')
         }
     })
 }
